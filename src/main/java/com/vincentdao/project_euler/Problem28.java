d20d6fe24f48333b877b2453a67e4a8b8dfa249d
vincentdao <vincentdaomain@gmail.com>
2023-09-25 11:52:24 +0700

    add problem 28

--- src/main/java/com/vincentdao/project_euler/Problem28.java
+++ src/main/java/com/vincentdao/project_euler/Problem28.java
@@ -2,7 +2,17 @@ package com.vincentdao.project_euler;
 
 public class Problem28 {
 
-    public static void main(String[] args) {
+    private static final int MAX_SIZE = 1001;
 
+    public static void main(String[] args) {
+        long sum = 1;
+        long startingNum = 1;
+        for (int size = 3; size <= MAX_SIZE; size += 2) {
+            for (int count = 0; count < 4; count++) {
+                startingNum += size - 1;
+                sum += startingNum;
+            }
+        }
+        System.out.println(sum);
     }
 }
